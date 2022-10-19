b69d1cfc79ea6d1f2c7ddeba4188e82406f9891d
Geilson <geilsonsrz@gmail.com>
2022-10-19 16:34:03 -0300

    Iniacao da estrutura logica

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,2 +1,12 @@
 "use strict";
-let n = 10;
+const telaSup = document.querySelector(".telaSup");
+const telaInt = document.querySelector(".telaInf");
+const botoes = document.querySelectorAll(".teclado button");
+class Calculadora {
+}
+botoes.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        var _a;
+        const valor = (_a = e.target) === null || _a === void 0 ? void 0 : _a.innerText;
+    });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-let n: number = 10
+const telaSup = document.querySelector(".telaSup")
+const telaInt = document.querySelector(".telaInf")
+const botoes = document.querySelectorAll(".teclado button")
+
+class Calculadora {
+
+}
+
+botoes.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+
+        const valor = e.target?.innerText
+
+
+    })
+})
