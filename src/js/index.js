e16b55cd7c52a90f2063017b9d9daf8ab2aa2735
Geilson <geilsonsrz@gmail.com>
2022-10-20 22:15:36 -0300

    Alteracoes 19-10

--- src/js/index.js
+++ src/js/index.js
@@ -4,13 +4,32 @@ const botoes = document.querySelectorAll(".teclado button");
 
 class Calculadora {
     constructor(telaSup, telaInf) {
-        this.telaSup = telaSup;
-        this.telaInf = telaInf;
+        this.telaSupTxt = telaSup;
+        this.telaInfTxt = telaInf;
+        this.telaInf = "";
     }
     //add digitos no visor da calculadora
     addDigito(digito) {
+        //Checagem para ponto decimal
+        if(digito === "." && this.telaInfTxt.innerText.includes(".")){
+            return;
+        }
+        this.telaInf = digito
+        this.updateTela()
+    }
+    //Processamento de operações
+    operacoes(op){
+        console.log(op)
+    }
+
+
+    //Atualização da tela
+    updateTela() {
+        this.telaInfTxt.innerText += this.telaInf
     }
 }
+
+const calc = new Calculadora(telaSup, telaInf)
 
 botoes.forEach((btn) => {
     btn.addEventListener("click", (e) => {
@@ -18,10 +37,10 @@ botoes.forEach((btn) => {
         const btn_valor = e.target?.innerText;
         //Identificando se o usuário clicou em número ou em operação.
         if (+btn_valor >= 0 || btn_valor === ".") {
-            console.log(btn_valor);
+            calc.addDigito(btn_valor);
         }
         else {
-            console.log("Op.: " + btn_valor);
+            calc.operacoes(btn_valor);
         }
     });
 });
